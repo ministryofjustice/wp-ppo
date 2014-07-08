caa208350f2c182d33a8c5d710c6dd3ffb7fe409
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-07-08 09:57:41 +0100

    Related content now displays page (not menu) siblings - page structure should be adjusted if necessary

--- wp-content/themes/ppo/templates/content-page.php
+++ wp-content/themes/ppo/templates/content-page.php
@@ -8,11 +8,15 @@
 	$all_wp_pages = $my_wp_query->query( array( 'post_type' => 'page', 'posts_per_page' => -1, 'order' => 'ASC', 'order_by' => 'position' ) );
 
 	// Filter through all pages and find Portfolio's children
-	$page_children = get_pages('child_of=' . get_the_ID() .'&hierarchical=0&parent=' . get_the_ID() . '&sort_column=menu_order');
+	// $page_children = get_pages('child_of=' . get_the_ID() .'&hierarchical=0&parent=' . get_the_ID() . '&sort_column=menu_order');
+	 
+	// Filter through all pages and find Portfolio's siblings
+	$page_parents = get_post_ancestors(get_the_ID());
+	$page_siblings = get_pages('child_of=' . $page_parents[0] .'&hierarchical=0&sort_column=menu_order');
 
-	if ( count( $page_children ) ) {
+	if ( count( $page_siblings ) ) {
 		echo "<div class='child-pages'>";
-		foreach ( $page_children as $child ) {
+		foreach ( $page_siblings as $child ) {
 			?>
 			<li>
 				<a href="<?php echo get_permalink( $child->ID ); ?>"><?php echo $child->post_title; ?></a>
