70318edd3d2b9c9ea954ea2ace12f3fcf1370955
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-07-18 14:37:19 +0100

    Establoshment filtering works plus maintenance utility for reattaching establishment IDs to documents (fixest)

--- wp-content/themes/ppo/lib/ajax.php
+++ wp-content/themes/ppo/lib/ajax.php
@@ -21,60 +21,79 @@ function update_tiles() {
 	// Converts dates to datetime for correct ordering
 	add_filter( 'posts_orderby', 'wdw_query_orderby_postmeta_date', 10, 1 );
 
+	// Stops the query if no $establishments are found
+	$stop_query = false;
+
 	$args_json = $_POST['queryParams'];
 
 	// Decode JSON to array
 	$args = json_decode( stripslashes( $args_json ), true );
 
-	foreach ( $args[tax_query] as $i => $tax_query ) {
-		if ( $tax_query[taxonomy] = "establishment-type" ) {
-			// Remove taxonomy query
-			unset($args[tax_query][$i]);
+	if ( isset( $args['tax_query'] ) ) {
+		foreach ( $args['tax_query'] as $i => $tax_query ) {
+			if ( $tax_query['taxonomy'] = "establishment-type" ) {
 
-			// Retrieve matching establishment IDs
-			$matching_establishments_args = array(
-				'post_type' => 'establishment',
-				'posts_per_page' => -1,
-				'tax_query' => array(
-					array(
-						'taxonomy' => 'establishment-type',
-						'terms' => $tax_query[terms]
+				// Remove taxonomy query
+				unset( $args['tax_query'][$i] );
+
+				// Retrieve matching establishment IDs
+				$matching_establishments_args = array(
+					'post_type' => 'establishment',
+					'posts_per_page' => -1,
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'establishment-type',
+							'terms' => $tax_query['terms']
+						)
 					)
-				)
-			);
+				);
 
-			$matching_establishments = new WP_Query( $matching_establishments_args );
+				$matching_establishments = new WP_Query( $matching_establishments_args );
 
-			while ( $matching_establishments->have_posts() ) {
-				$matching_establishments->the_post();
-				$matching_establishments_ids[] = get_the_ID();
+				$matching_establishments_ids = array();
+				while ( $matching_establishments->have_posts() ) {
+					$matching_establishments->the_post();
+					$matching_establishments_ids[] = get_the_ID();
+				}
+
+				if ( count( $matching_establishments_ids ) == 0 ) {
+					$stop_query = true;
+				}
+
+				// Add custom field query
+				$args['meta_query'][] = array(
+					'key' => 'fii-establishment',
+					'value' => $matching_establishments_ids,
+					'compare' => 'IN',
+					'include_children' => false
+				);
+
+				wp_reset_postdata();
 			}
-
-			// Add custom field query
-			$args[meta_query][] = array(
-				'key' => 'fii-establishment',
-				'value' => $matching_establishments_ids,
-				'compare' => 'IN',
-				'include_children' => false
-			);
-
-			wp_reset_postdata();
 		}
 	}
 
 	$ajax_query = new WP_Query( $args );
-	ob_start();
-?>
-	<?php while ( $ajax_query->have_posts() ) : $ajax_query->the_post(); ?>
-		<?php get_template_part( 'templates/content-tile', get_post_format() ); ?>
-	<?php endwhile; ?>
-	<?php
+	if ( $ajax_query->have_posts() && !$stop_query ) {
+		ob_start();
+		?>
+		<?php while ( $ajax_query->have_posts() ) : $ajax_query->the_post(); ?>
+			<?php get_template_part( 'templates/content-tile', get_post_format() ); ?>
+		<?php endwhile; ?>
+		<?php
 
-	$tile_output = ob_get_contents();
-	ob_end_clean();
-	echo $tile_output;
-	remove_filter( 'posts_orderby', 'wdw_query_orderby_postmeta_date', 10, 1 );
-	die();
+		$tile_output = ob_get_contents();
+		ob_end_clean();
+		if ( $tile_output ) {
+			echo $tile_output;
+		}
+
+		remove_filter( 'posts_orderby', 'wdw_query_orderby_postmeta_date', 10, 1 );
+		die();
+	} else {
+		echo "<h2>No results found</h2>";
+		die();
+	}
 }
 
 add_action( 'wp_ajax_nopriv_update_tiles', 'update_tiles' );
--- wp-content/themes/ppo/templates/content-tile.php
+++ wp-content/themes/ppo/templates/content-tile.php
@@ -1,4 +1,5 @@
-<?php
+<?php 
+// Set classes for tile based in document type (there should only be one)
 $doc_type_array = get_the_terms( get_the_ID(), 'document_type' );
 $doc_types = array();
 foreach ( $doc_type_array as $doc_type ) {
@@ -10,45 +11,89 @@ $document_date = get_metadata( 'post', get_the_ID(), 'document-date', true );
 $document_datetime = date( "Y-m-d", strtotime( str_replace( "/", "-", $document_date ) ) );
 $document_decade = substr( $document_datetime, 0, 3 ) . "0";
 
+$death_date = get_metadata( 'post', get_the_ID(), 'fii-death-date', true );
+
 $document_size = get_filesize( get_metadata( 'post', get_the_ID(), 'document-upload', true ) );
 
-$document_establishment = get_metadata( 'post', get_the_ID(), 'fii-establishment', true );
-$document_establishment_type = get_metadata( 'post', $document_establishment, 'establishment-type', true );
+// Set flags for determining document type
+$is_fii = ($doc_types[0] == "fii-report" ? true : false);
 
-$document_death_types = wp_get_post_terms( get_the_ID(), 'fii-death-type', array( "fields" => "ids" ) );
-if ( isset( $document_death_types[0] ) ) {
-	$document_death_type = $document_death_types[0];
-} else {
-	$document_death_type = "";
+// Conditionals for document type
+if ( $is_fii ) {
+	$document_establishment_id = get_metadata( 'post', get_the_ID(), 'fii-establishment', true );
+	$document_establishment_type = get_metadata( 'post', $document_establishment_id, 'establishment-type', true );
+
+	$document_death_types = wp_get_post_terms( get_the_ID(), 'fii-death-type', array( "fields" => "ids" ) );
+	if ( isset( $document_death_types[0] ) ) {
+		$document_death_type = $document_death_types[0];
+	} else {
+		$document_death_type = "";
+	}
 }
 
+// Construct the tile metadata
 $tile_data = " data-date='" . $document_datetime . "'"
 		. " data-size='" . $document_size . "'"
 		. " data-decade='" . $document_decade . "'"
-		. " data-fii-death-type='" . str_replace( " ", "-", strtolower( $document_death_type ) ) . "'"
-		. " data-establishment-type='" . $document_establishment_type . "'";
+		. ($is_fii ? " data-fii-death-type='" . str_replace( " ", "-", strtolower( $document_death_type ) ) . "'" : "")
+		. ($is_fii ? " data-establishment-type='" . $document_establishment_type . "'" : "");
 ?>
 
 <article id="<?php echo 'doc-' . get_the_ID(); ?>" class="<?php echo $doc_classes; ?>"<?php echo $tile_data; ?>>
-	<div class="tile-image">
-		<a href="<?php echo get_metadata( 'post', get_the_ID(), 'document-upload', true ); ?>">
-			<?php the_post_thumbnail(); ?>
-		</a>
-	</div>
-	<div class="tile-details">
-		<?php if ( !in_array( 'fii-report', $doc_types ) ) { ?>
-			<h3><?php the_title(); ?></h3>
-		<?php } else { ?>
-			<h4><?php echo ot_echo_option( 'fii-establishment' ); ?></h4>
+	<a href="<?php echo get_metadata( 'post', get_the_ID(), 'document-upload', true ); ?>">
+		<?php if ( !$is_fii ) { ?>
+			<div class="tile-image">
+				<?php the_post_thumbnail(); ?>
+			</div>
 		<?php } ?>
-		<h4><?php echo $document_date; ?></h4>
-		<div class="tile-size">
-			<?php echo $document_size; ?>
+		<?php if ( !$is_fii ) { ?>
+			<div class="tile-details">
+				<h3><?php the_title(); ?></h3>
+				<h4>Published: <?php echo $document_date; ?></h4>
+			</div>
+		<?php } else { ?>
+			<h3><?php echo get_the_title( $document_establishment_id ) ? get_the_title( $document_establishment_id ) : "&nbsp;"; ?></h3>
+			<div class="tile-details">
+				<table>
+					<tr>
+						<td><strong>
+							<?php
+							if ( $document_establishment_type ) {
+								echo get_term_field( "name", $document_establishment_type, 'establishment-type' );
+							}
+							?>
+							</strong></td>
+					</tr>
+					<tr>
+						<td>Published:</td> 
+						<td><?php echo $document_date; ?></td>
+					</tr>
+					<tr>
+						<td>Date of death:</td>
+						<td><?php echo $death_date; ?></td>
+					</tr>
+					<tr>
+						<td>Type of death:</td>
+						<td><?php echo get_term_field( "name", $document_death_type, 'fii-death-type' ); ?></td>
+					</tr>
+					<tr>
+						<td>&nbsp;</td>
+					</tr>
+					<tr>
+						<td><?php echo (get_metadata( 'post', get_the_ID(), 'fii-gender', true ) == "m" ? "Male" : "Female"); ?></td>
+						<td><?php echo get_metadata( 'post', get_the_ID(), 'fii-age', true ); ?></td>
+					</tr>
+				</table>
+			</div>
+		<?php } ?>
+		<!--		<div class="tile-size">
+		<?php echo $document_size; ?>
+				</div>-->
+		<!--		<div class="tile-social">
+					<a href="//twitter.com/intent/tweet?url=<?php echo get_metadata( 'post', get_the_ID(), 'document-upload', true ); ?>&text=<?php echo get_the_title(); ?>&via=sparkdevelop" target="_blank">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/twitter.png" alt="Share on Twitter" title="Share on Twitter">
+					</a>
+				</div>-->
 		</div>
-		<div class="tile-social">
-			<a href="//twitter.com/intent/tweet?url=<?php echo get_metadata( 'post', get_the_ID(), 'document-upload', true ); ?>&text=<?php echo get_the_title(); ?>&via=sparkdevelop" target="_blank">
-				<img src="<?php echo get_template_directory_uri(); ?>/assets/icons/twitter.png" alt="Share on Twitter" title="Share on Twitter">
-			</a>
-		</div>
-	</div>
+	</a>
 </article>
