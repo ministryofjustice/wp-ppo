f15c1b8952705bbd1b08e89abadd6aa5a0f29263
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-07-21 13:25:01 +0100

    Replace logo with hi-res version

--- wp-content/themes/ppo/templates/header.php
+++ wp-content/themes/ppo/templates/header.php
@@ -1,7 +1,7 @@
 <header class="banner container" role="banner">
 	<div class="row">
 		<div class="col-lg-12">
-			<a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/ppo-logo_screen.png" alt="<?php bloginfo( 'name' ); ?>"></a>
+			<a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/ppo-logo_white.png" alt="<?php bloginfo( 'name' ); ?>"></a>
 			<nav class="nav-main" role="navigation">
 				<?php
 				if ( has_nav_menu( 'primary_navigation' ) ) :
