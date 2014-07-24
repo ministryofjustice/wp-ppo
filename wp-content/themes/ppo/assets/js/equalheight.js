ea91becd3dc5c89d26cd304d172fbc003f7c0706
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-07-24 12:12:43 +0100

    Improved equalheight.js

--- wp-content/themes/ppo/assets/js/equalheight.js
+++ wp-content/themes/ppo/assets/js/equalheight.js
@@ -1,60 +1,45 @@
-function setConformingHeight(el, newHeight) {
-    // set the height to something new, but remember the original height in case things change
-    el.data("originalHeight", (el.data("originalHeight") == undefined) ? (el.height()) : (el.data("originalHeight")));
-    el.height(newHeight);
-//    console.log(el);
-//    console.log(el.height());
-}
+equalheight = function(container){
 
-function getOriginalHeight(el) {
-    // if the height has changed, send the originalHeight
-    return (el.data("originalHeight") == undefined) ? (el.height()) : (el.data("originalHeight"));
-}
+var currentTallest = 0,
+     currentRowStart = 0,
+     rowDivs = new Array(),
+     $el,
+     topPosition = 0;
+ $(container).each(function() {
 
-function columnConform(selector) {
+   $el = $(this);
+   $($el).height('auto')
+   topPostion = $el.position().top;
 
-    var currentTallest = 0;
-    var currentRowStart = 0;
-    var rowDivs = new Array();
-
-    // find the tallest DIV in the row, and set the heights of all of the DIVs to match it.
-    $(selector).each(function(index) {
-
-        if (currentRowStart != $(this).position().top) {
-
-            // we just came to a new row.  Set all the heights on the completed row
-            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++)
-                setConformingHeight(rowDivs[currentDiv], currentTallest);
-
-            // set the variables for the new row
-            rowDivs.length = 0; // empty the array
-            currentRowStart = $(this).position().top;
-            currentTallest = getOriginalHeight($(this));
-            rowDivs.push($(this));
-
-        } else {
-
-            // another div on the current row.  Add it to the list and check if it's taller
-            rowDivs.push($(this));
-            currentTallest = (currentTallest < getOriginalHeight($(this))) ? (getOriginalHeight($(this))) : (currentTallest);
-
-        }
-        // do the last row
-        for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++)
-            setConformingHeight(rowDivs[currentDiv], currentTallest);
-
-    });
-
-}
+   if (currentRowStart != topPostion) {
+     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+       rowDivs[currentDiv].height(currentTallest);
+     }
+     rowDivs.length = 0; // empty the array
+     currentRowStart = topPostion;
+     currentTallest = $el.height();
+     rowDivs.push($el);
+   } else {
+     rowDivs.push($el);
+     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+  }
+   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+     rowDivs[currentDiv].height(currentTallest);
+   }
+ });
+};
 
 $(window).load(function() {
-    columnConform('nav>ul>li>a');
-    columnConform('#home-cta-container .cta-inner');
-    columnConform('#home-cta-container .cta-inner h2');
+//    columnConform('nav>ul>li>a');
+//    columnConform('#home-cta-container .cta-inner h2');
+//    columnConform('#home-cta-container .cta-inner');    
+    equalheight('nav>ul>li>a');
+    equalheight('#home-cta-container .cta-inner h2');
+    equalheight('#home-cta-container .cta-inner'); 
 });
 
-window.onresize = function() {
-    columnConform('nav>ul>li>a');
-    columnConform('#home-cta-container .cta-inner');
-    columnConform('#home-cta-container .cta-inner h2');
-};
+$(window).resize(function() {
+    equalheight('nav>ul>li>a');
+    equalheight('#home-cta-container .cta-inner h2');
+    equalheight('#home-cta-container .cta-inner'); 
+});
