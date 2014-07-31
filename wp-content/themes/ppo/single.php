f2483f7cb0c24ed59ceb1ffd80ed291cd74a8f59
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-07-31 11:45:25 +0100

    Tidied up single post page

--- wp-content/themes/ppo/single.php
+++ wp-content/themes/ppo/single.php
@@ -1,1 +1,10 @@
-<?php get_template_part('templates/content', 'single'); ?>
+<?php get_template_part( 'templates/content', 'single' ); ?>
+
+<div class="navigation">
+	<div class="textleft col-md-6">
+		<?php previous_post_link("<span class='glyphicon glyphicon-chevron-left'></span> %link"); ?>
+	</div>
+	<div class="textright col-md-6">
+		<?php next_post_link("%link <span class='glyphicon glyphicon-chevron-right'></span>"); ?>
+	</div>
+</div> <!-- end navigation -->
