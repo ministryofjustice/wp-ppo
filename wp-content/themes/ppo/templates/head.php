c7c998fc51d0935ca766fae5740da5cd37b352e9
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-08-27 12:53:38 +0100

    Fonts brought into project

--- wp-content/themes/ppo/templates/head.php
+++ wp-content/themes/ppo/templates/head.php
@@ -9,8 +9,8 @@
 		<?php wp_head(); ?>
 
 		<!-- Google fonts -->
-		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
-		<link href='http://fonts.googleapis.com/css?family=Gentium+Book+Basic' rel='stylesheet' type='text/css'>
+		<link href='<?php echo get_template_directory_uri(); ?>/assets/css/Roboto.css' rel='stylesheet' type='text/css'>
+		<link href='<?php echo get_template_directory_uri(); ?>/assets/css/GentiumBookBasic.css' rel='stylesheet' type='text/css'>
 
 		<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> Feed" href="<?php echo esc_url( get_feed_link() ); ?>">
 
