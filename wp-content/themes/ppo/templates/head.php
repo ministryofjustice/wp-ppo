2a7ae4264ee08985353780f743d55efc394da051
ryanajarrett <ryan.jarrett@digital.justice.gov.uk>
2014-08-27 16:17:56 +0100

    Added IE7 fontawesome support

--- wp-content/themes/ppo/templates/head.php
+++ wp-content/themes/ppo/templates/head.php
@@ -14,6 +14,10 @@
 
 		<!--<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> Feed" href="<?php echo esc_url( get_feed_link() ); ?>">-->
 
+		<!--[if IE 7]>
+			<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/font-awesome-ie7.min.css">
+		<![endif]-->
+
 		<!--[if IE 8]>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/respond.js"></script>
 		<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/assets/js/EventHelpers.js"></script>
