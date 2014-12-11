7dd45c16b491ccf102e5b92201e7cc6ff400c50f
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2014-12-11 14:14:11 +0000

    Changed method by which Roboto font is requested to improve browser support. Deleted unneeded Roboto.css file and related reference

--- wp-content/themes/ppo/templates/head.php
+++ wp-content/themes/ppo/templates/head.php
@@ -9,7 +9,6 @@
 		<?php wp_head(); ?>
 
 		<!-- Google fonts -->
-		<link href='<?php echo get_template_directory_uri(); ?>/assets/css/Roboto.css' rel='stylesheet' type='text/css'>
 		<link href='<?php echo get_template_directory_uri(); ?>/assets/css/GentiumBookBasic.css' rel='stylesheet' type='text/css'>
 
 		<!--<link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo( 'name' ); ?> Feed" href="<?php echo esc_url( get_feed_link() ); ?>">-->
