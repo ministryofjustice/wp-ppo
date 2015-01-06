e27a8ce63f644b4d3bc9d76ad9cd154bb6f9366b
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-01-06 14:23:12 +0000

    Revert "Changed default ordering of FII report tiles."
    This reverts commit e9f9d46be3bc4a8d58a02fd062f09ae2ab757f73.

--- wp-content/themes/ppo/taxonomy-document_type.php
+++ wp-content/themes/ppo/taxonomy-document_type.php
@@ -25,7 +25,7 @@
 				'establishment' => 'autocomplete'
 			),
 			'sort' => array( 'publish-date', 'date-of-death' ),
-			'default' => 'date-of-death'
+			'default' => 'publish-date'
 		)
 	);
 
@@ -220,7 +220,7 @@
 				'paged' => $paged,
 				'order' => "DESC",
 				'orderby' => 'meta_value',
-				'meta_key' => 'fii-death-date'
+				'meta_key' => 'document-date'
 			) ) );
 	echo "<script type = 'text/javascript'>curPage = $paged;
 							maxPage = " . ceil( $wp_query->found_posts / $post_per_page ) . ";
