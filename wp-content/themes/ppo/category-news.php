a4355ffd014b8c26f71220a31202cd69402491f5
Dylan Williams <dylan.williams@digital.justice.gov.uk>
2015-02-12 15:24:40 +0000

    Revert "Added thumbnails to news archive page"
    This reverts commit 89af94455e3a785863430be5a5887d5a37a188ba.

--- wp-content/themes/ppo/category-news.php
+++ wp-content/themes/ppo/category-news.php
@@ -14,25 +14,16 @@ Template Name: Category news archive
 <div class="news-archive-links">
 	<ul>
 
-
 		<?php 
 			$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 			query_posts('cat=news&posts_per_page=10&paged=' . $paged); ?>
 		<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-
-
-		  <li>
-		  <a href="<?php the_permalink() ?>"><?php the_post_thumbnail( 'home-news-thumb' ); ?></a>
-		  <h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+		  <li><h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
 		  <time class="published"><?php echo get_the_date(); ?></time>
-
 		    <?php the_excerpt() ?>
-		    
 		  </li>
 		  <hr/>
 	    <?php endwhile; endif; ?>
-
-
 	</ul>
 
 <div class="pagination">
