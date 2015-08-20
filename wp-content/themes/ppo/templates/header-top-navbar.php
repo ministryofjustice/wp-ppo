fc7f252369d619332df30e48689f27232ea75cac
Ollie Treend <ollie.treend@digital.justice.gov.uk>
2015-08-20 17:00:43 +0100

    Remove commented HTML menu code.

--- wp-content/themes/ppo/templates/header-top-navbar.php
+++ wp-content/themes/ppo/templates/header-top-navbar.php
@@ -1,38 +1,3 @@
-<!-- <header class="banner navbar navbar-default navbar-static-top" role="banner">
-
-	<div class="nav-container">
-		<a class="brand" href="<?php echo home_url(); ?>/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/ppo-logo_white.png" alt="<?php bloginfo( 'name' ); ?>"></a>
-		<nav class="collapse navbar-collapse" role="navigation">
-			<?php
-			if ( has_nav_menu( 'primary_navigation' ) ) :
-				wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'depth' => 3 ) );
-			endif;
-			?>
-		</nav>
-	</div>
-	<div class="container">
-		<div class="navbar-header">
-			<button id="trigger" type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-				<span class="sr-only">Toggle navigation</span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-		</div>
-	</div>
-</header>
-
-<div class="mp-pusher" id="mp-pusher">
-	<nav id="mp-menu" class="mp-menu">
-		<?php
-		if ( has_nav_menu( 'primary_navigation' ) ) :
-			wp_nav_menu( array( 'theme_location' => 'primary_navigation', 'menu_class' => 'nav navbar-nav', 'depth' => 3, 'walker' => new Mob_Nav_Walker ) );
-		endif;
-		?>
-	</nav>
-
-</div> -->
-
 <header class="banner navbar navbar-static-top" role="banner">
 	<div class="nav-container">
 		<a class="brand" href="<?php echo home_url(); ?>/">
