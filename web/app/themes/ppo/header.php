102c12e3607ffc63f0b3f1db0cc2f3f2c3bd7415
Beverley Newing <bnewing92@hotmail.co.uk>
2021-01-22 11:45:54 +0000

    Pull in new cookie banner
    This pulls in cookie banner 2.0.1

--- web/app/themes/ppo/header.php
+++ web/app/themes/ppo/header.php
@@ -36,6 +36,18 @@
 
 <body <?php body_class(); ?>>
 
+<?php
+if ( ! function_exists( 'wp_body_open' ) ) {
+	/**
+	 * Open the body tag, pull in any hooked triggers.
+	 **/
+	function wp_body_open() {
+		do_action( 'wp_body_open' );
+	}
+}
+wp_body_open();
+?>
+
 <a class="skip-main" href="#content">Skip to main content</a>
 <div id="shade-overlay" class=""></div>
 
